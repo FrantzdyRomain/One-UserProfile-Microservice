791f159cb277ff2992a4415634fb7ffe257888f4
Frantzdy romain <Frantzdyromain@gmail.com>
2017-12-05 23:36:13 -0500

    Repository added.

---
+++ userprofile-service/repository/userrepository.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+var UserRepository = {
+    findByEmail: function(email) {
+        return db.User.findAll({
+            where: {
+                email: email
+            }
+        })
+    },
+
+    findUserByUsername: function(username){
+        return User.findById(username);
+    }
+}
+
+module.exports = Usersepository;
